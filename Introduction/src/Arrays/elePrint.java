d48f670753cc9e620ae573ceeb50b418c33f91e3
Madhumita Goswami <madhumita7301jsr@gmail.com>
2020-08-05 06:37:34 +0530

    evenPositionElements

---
+++ Introduction/src/Arrays/elePrint.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class elePrint {
+
+	public static void main(String[] args) {
+		// Printing the array elements.
+		
+		int arr[] = {1,2,3,4,5,6,7,8,9};
+		
+		System.out.println("The elements of the array are :");
+		
+		for(int i =0; i<arr.length; i++) {
+			System.out.print(arr[i] + "   ");
+		}
+
+	}
+
+}
